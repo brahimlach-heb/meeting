094dc69332afe7e40c669ad855ff4c4469887095
brahim <brahimlachheb58@gmail.com>
2023-08-01 19:55:32 +0100

    connect to mysql

---
+++ src/main/java/com/group/meeting/models/Meeting_table.java
@@ -0,0 +1,69 @@
+package com.group.meeting.models;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+@Entity
+@Table(name = "meetings")
+public class Meeting_table {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "start_time")
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time")
+    private LocalDateTime endTime;
+
+
+
+    @ElementCollection
+    @CollectionTable(name = "meeting_attendees", joinColumns = @JoinColumn(name = "meeting_id"))
+    @Column(name = "attendee")
+    private List<String> attendees;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public List<String> getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(List<String> attendees) {
+        this.attendees = attendees;
+    }
+}
+
