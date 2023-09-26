d525acef71a25d18d5f9fe1a1b63a0b8a172c680
brahim <brahimlachheb58@gmail.com>
2023-09-27 00:05:45 +0100

    chat

--- EverMeet0.2/src/components/pages/Chat.js
+++ EverMeet0.2/src/components/pages/Chat.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Sidebar from '../Sidebar';
 import Footer from '../Footer';
 import Navbar from '../Navbar';
+import ChatMessage from '../Chat';
 
 function Chat() {
   return (
     <>
     <Navbar/>
-    <Sidebar />
-    <Footer />
+    <ChatMessage />
+    
     </>
   );
 }
